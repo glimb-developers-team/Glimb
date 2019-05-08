9651f2bb386b14b5cea0e51e2e11a001c400a4e1
ScrappyDo <ScrappyDo@users.noreply.github.com>
2019-05-08 20:47:18 +0300

    Removed the opening of the client window after registration

--- src/RegistrationWindow.cpp
+++ src/RegistrationWindow.cpp
@@ -54,10 +54,8 @@ void RegistrationWindow::on_Button_RegistrationTwo_clicked()
                             type,
                                             ui->lineEdit_PhoneContractor->text().toUtf8().constData()
                                             );
-                 sWindow = new ClientWindow (0, ui->lineEdit_Name->text().toUtf8().constData(),  ui->lineEdit_LastName->text().toUtf8().constData(),
-                                            ui->lineEdit_MiddleName->text().toUtf8().constData());
-
-                 sWindow->show();
+                 //sWindow = new ClientWindow (0, ui->lineEdit_Name->text().toUtf8().constData(),  ui->lineEdit_LastName->text().toUtf8().constData(),
+                                           // ui->lineEdit_MiddleName->text().toUtf8().constData());
                  this->close();
               }
         catch (char const *error)
