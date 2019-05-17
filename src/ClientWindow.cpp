7cddf9c8ac9b34bd0b1bf61da252fefebf29193b
ScrappyDo <ScrappyDo@users.noreply.github.com>
2019-05-17 14:23:03 +0300

    Huinya#2

--- src/ClientWindow.cpp
+++ src/ClientWindow.cpp
@@ -14,7 +14,19 @@ ClientWindow::ClientWindow(QWidget *parent, std::string name, std::string last_n
     std::string FIO = name;
     last_name + middle_name;
     ui->label_FIO->setText(FIO.c_str());
+    for (int g = 1; g < 6; g++)
+    {
+        QTreeWidgetItem *i = new QTreeWidgetItem(ui->treeWidget);
+            ui->treeWidget->addTopLevelItem(i);
+            i->setText(0,QString::fromStdString(std::string("Закупка")+std::to_string(g)));
+        for(int k = 1; k < 10; k++)
+        {
+            QTreeWidgetItem *item = new QTreeWidgetItem(i);
+            item->setText(0,QString::fromStdString(std::string("Материалы")+std::to_string(k)));
+        }
+    }
 }
+
 
 ClientWindow::~ClientWindow()
 {
